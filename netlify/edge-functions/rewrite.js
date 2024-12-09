b643c62f0f17546479a47156549bf2b8cb44df3a
YujohnNattrass <46704329+YujohnNattrass@users.noreply.github.com>
2024-12-09 16:35:22 -0500

    Update rewrite.js

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,5 +1,6 @@
 export default async (request, context) => {
     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+    console.log(`###`, { host: request.headers.get(host) })
     return Response.redirect(new URL(path, request.url));
 };
 
